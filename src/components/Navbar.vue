16c67785f9abfda6a38fbb7b72a4af6b3b06798f
KirillZ <kzasorin@gmail.com>
2019-06-03 12:17:50 +0300

    Добавлен Navbar и логотип с кноплкой Выйти

---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav>
+    <v-toolbar flat app>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">Моё</span>
+        <span>Приложение</span>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      
+      <v-btn flat color="grey">
+        <span>Выйти</span>
+        <v-icon>exit_to_app</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </nav>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
